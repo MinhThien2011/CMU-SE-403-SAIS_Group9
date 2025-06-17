6e0bfc6c14f8a9f399a9d00186d037f163ae0128
quanganh <anhpk2310@gmail.com>
2025-06-17 17:27:09 +0700

    fibonscci

---
+++ services/fibonacci.js
@@ -0,0 +1,9 @@
+export const fibonacci = (n) => {
+  const result = [];
+  let a = 0, b = 1;
+  for (let i = 0; i < n; i++) {
+    result.push(a);
+    [a, b] = [b, a + b];
+  }
+  return result;
+};
